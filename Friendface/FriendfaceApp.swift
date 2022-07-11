16f3bcef7d7703051f073249d1b2884984685893
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-07-11 19:09:37 -0400

    Initial Commit

---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Gabriel Marquez on 2022-07-11.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
